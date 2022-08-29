99afabcc88a033ee475a646d766dd089da8a65bc
AlfredoPazZamora <apaz0@ucol.mx>
2022-08-29 12:41:13 -0500

    Feat: Install and configure apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import client from '../src/apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+
+  // return <Component {...pageProps} />
 }
 
 export default MyApp
