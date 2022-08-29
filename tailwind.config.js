fe1fd1f7700c5199af97921203258e3f267cd1f7
AlfredoPazZamora <apaz0@ucol.mx>
2022-08-29 12:06:11 -0500

    Feat: Customize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '375px',
+      'md': '500px',
+      'lg': '1024px'
+    },
+
+    colors: {
+      blue: "#31D0C1",
+      green: "#8DE137",
+      black: "#000000",
+      white: "#FEFEFE"
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    fontSize: {
+      h1: "5rem",
+      h2: "2.5rem",
+      h3: "2rem",
+      base: "1.6rem",
+      small: "1.2rem",
+    },
     extend: {},
   },
   plugins: [],
