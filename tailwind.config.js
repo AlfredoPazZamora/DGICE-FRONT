b00c137c5db048920507fbe147e82075de5ee1cf
AlfredoPazZamora <apaz0@ucol.mx>
2022-08-29 12:14:56 -0500

    Change customize names

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,20 +12,20 @@ module.exports = {
     },
 
     colors: {
-      blue: "#31D0C1",
-      green: "#8DE137",
-      black: "#000000",
-      white: "#FEFEFE"
+      'blue': "#31D0C1",
+      'green': "#8DE137",
+      'black': "#000000",
+      'white': "#FEFEFE"
     },
     fontFamily: {
       inter: ["Inter", "sans-serif"],
     },
     fontSize: {
-      h1: "5rem",
-      h2: "2.5rem",
-      h3: "2rem",
-      base: "1.6rem",
-      small: "1.2rem",
+      'h1': "5rem",
+      'h2': "2.5rem",
+      'h3': "2rem",
+      'base': "1.6rem",
+      'small': "1.2rem",
     },
     extend: {},
   },
