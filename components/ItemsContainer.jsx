aeccc50332756449e3c0849bca4b4d7dfaacd1fc
josechuy123 <jdiaz33@ucol.mx>
2022-08-30 00:31:39 -0500

    color-footer

--- components/ItemsContainer.jsx
+++ components/ItemsContainer.jsx
@@ -5,14 +5,14 @@ import ImgSEP from './ImgSEP'
 
 const ItemsContainer = () => {
     return (
-        <div>
-            <div className="bg-blue-900 text-black p-20 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-40 sm:px-8 px-5 py-10">
+        <div className=" ">
+            <div className="bg-blue  text-black p-20 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-40 sm:px-8 px-5 py-10">
                 <Item />
                 <ImgFeuc />
                 <ImgSEP />
                 <Item2 />
             </div>
-            <div className="text-center ">
+            <div className="text-center bg-blue ">
                 <h1 className="text-center bg-blue-900 text-black p-6">© Derechos Reservados Universidad de Colima</h1>
             </div>
         </div>
