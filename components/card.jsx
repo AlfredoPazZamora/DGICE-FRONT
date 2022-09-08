66404bb5e76e4353c659b141d97b0521d85a43ba
AlfredoPazZamora <apaz0@ucol.mx>
2022-09-08 16:27:38 -0500

    Feat: Card botom
    - Add button and text

--- components/card.jsx
+++ components/card.jsx
@@ -1,5 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Script from 'next/script'
 
 export const Card = () => {
+    const value = 5;
 	return (
         <>
             <article className="w-4/5 lg:w-1/3 m-5 flex flex-col border rounded-xl box-border">
@@ -8,25 +12,33 @@ export const Card = () => {
 
                     {/* Text container */}
                     <div className="p-5 backdrop-blur-xl bg-white-1/2 rounded-tr-xl rounded-tl-xl">
-                        <p className="mb-5 text-sm text-white z-10 font-thin uppercase tracking-widest">Curso</p>
+                        <p className="mb-5 text-sm text-white/80 z-10 font-thin uppercase tracking-widest">Curso</p>
                         <div className="flex place-items-center mb-5">
                             <picture className="w-1/4">
                                 <img className="w-14 h-14 rounded-full" src="https://picsum.photos/seed/picsum/200/300" alt="Imagen del curso" />
                             </picture>
-                            <h3 className="ml-5 text-base w-3/4">Nombre del curso</h3>
+                            <h3 className="ml-5 text-base w-3/4 text-black font-bold">Nombre del curso</h3>
                         </div>
-                        <div className="mb-5">
-                            <p>Stars</p>
+                        <div className="mb-5 flex place-items-center">
+                            <p className="mr-2 text-white/80 font-bold">4</p>
+                            <FontAwesomeIcon icon={ faStar } className="text-[#ffd60a]" />
+                            <FontAwesomeIcon icon={ faStar } className="text-[#ffd60a]" />
+                            <FontAwesomeIcon icon={ faStar } className="text-[#ffd60a]" />
+                            <FontAwesomeIcon icon={ faStar } className="text-[#ffd60a]" />
                         </div>
 
-                        <p className="text-sm text-gray">2 horas</p>
+                        <p className="text-sm text-white/80 font-bold">2 horas</p>
                     </div>
                 </section>
 
                 {/* Bottom card */}
-                <section>
-                    <p>Profesor</p>
-                    <a>Button</a>
+                <section className=" rounded-br-xl rounded-bl-xl p-5 flex flex-col">
+                    <p className="font-bold text-small tracking-wide mb-4">Profesor</p>
+                    <a className="border w-full text-center py-2 rounded-md font-bold flex justify-center">
+                        <span>Button</span>
+                        <span className="ml-2 "> <FontAwesomeIcon icon={faArrowRight}/> </span> 
+                    </a>
+                
                 </section>
 
             </article>
