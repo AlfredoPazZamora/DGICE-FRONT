8afc34931a78324af0548f06c067986228111083
AlfredoPazZamora <apaz0@ucol.mx>
2022-09-09 12:38:05 -0500

    Create a file to render all courses

---
+++ components/Card/ListCards.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { Card } from './Card'
+
+const values = [
+    {
+        id: 1,
+        title: "Curso de CSS",
+        image: "https://picsum.photos/200/300",
+        teacher: "Victor Robles",
+        rating: 4,
+        duration: "2 horas",
+    },
+    {
+        id: 2,
+        title: "Curso de HTML",
+        image: "https://picsum.photos/200/300",
+        teacher: "Victor Robles",
+        rating: 4,
+        duration: "3 horas",
+    },
+    {
+        id: 3,
+        title: "Curso de JS",
+        image: "https://picsum.photos/200/300",
+        teacher: "Victor Robles",
+        rating: 4,
+        duration: "4 horas",
+    },
+    {
+        id: 4,
+        title: "Curso de SASS",
+        image: "https://picsum.photos/200/300",
+        teacher: "Victor Robles",
+        rating: 4,
+        duration: "5 horas",
+    },
+    {
+        id: 5,
+        title: "Curso de React",
+        image: "https://picsum.photos/200/300",
+        teacher: "Victor Robles",
+        rating: 4,
+        duration: "6 horas",
+    }
+];
+export const ListCards = () => {
+
+
+
+
+
+	return (
+        <>
+            {/* List cards */}
+            <section className=' grid w-full gap-8 bg-white grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 justify-items-center'>
+
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </section>
+        </>
+    );
+};
