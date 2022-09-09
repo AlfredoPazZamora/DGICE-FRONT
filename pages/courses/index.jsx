6480968c991b96184d007d6a1d700f321552a94d
AlfredoPazZamora <apaz0@ucol.mx>
2022-09-09 12:40:14 -0500

    Feat: Create a directory to add link to courses

--- pages/courses/index.jsx
+++ pages/courses/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import { Card } from "../../components/Card/Card";
 
 import { ListCards } from '../../components/Card/ListCards'
 
@@ -12,14 +13,17 @@ const allCourses = () => {
             </Head>
 
 
-            <main className="flex flex-col lg:flex-row ">
-                <aside className="lg:flex bg-blue hidden lg:w-2/12 lg:justify-center lg:p-5">
-                    <p>Lateral</p>
-                </aside>
+            <main className=" w-full border flex justify-center">
 
-                <section className="w-full lg:w-10/12">
-                    <ListCards />
-                </section>
+                <div className="w-full lg:w-10/12 lg:flex ">
+                    <aside className=" bg-white border rounded-xl h-fit mt-5 lg:sticky lg:top-5 hidden lg:flex lg:w-2/12 lg:justify-center lg:p-5">
+                        <p>Lateral</p>
+                    </aside>
+
+                    <section className="w-full p-5 lg:w-10/12 bg-white flex justify-center">
+                        <ListCards />
+                    </section>
+                </div>
 
             </main>
 
