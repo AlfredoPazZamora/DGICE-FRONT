57f8086c7e82974f1b09c6fcff052cf386a6f04a
AlfredoPazZamora <apaz0@ucol.mx>
2022-09-09 12:37:26 -0500

    Update name and styles to be responsive

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -2,11 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Script from 'next/script'
 
-export const Card = () => {
+export const Card = ({key}) => {
+    // console.log(key);
     const value = 5;
 	return (
         <>
-            <article className="w-4/5 lg:w-1/3 m-5 flex flex-col border rounded-xl box-border">
+            <article className="w-full lg:max-w-xs flex flex-col rounded-xl box-border shadow-xl">
                 {/* Top card */}
                 <section className="rounded-tr-xl rounded-tl-xl bg-[url('https://victorroblesweb.es/wp-content/uploads/2020/03/Aprender-CSS-en-15-minutos.png')] bg-cover ">
 
@@ -15,9 +16,9 @@ export const Card = () => {
                         <p className="mb-5 text-sm text-white/80 z-10 font-thin uppercase tracking-widest">Curso</p>
                         <div className="flex place-items-center mb-5">
                             <picture className="w-1/4">
-                                <img className="w-14 h-14 rounded-full" src="https://picsum.photos/seed/picsum/200/300" alt="Imagen del curso" />
+                                <img className="w-14 h-14 rounded-full" src="https://picsum.photos/200/300" alt="Imagen del curso" />
                             </picture>
-                            <h3 className="ml-5 text-base w-3/4 text-black font-bold">Nombre del curso</h3>
+                            <h3 className="ml-3 text-base w-3/4 text-black font-regular ">Nombre del curso</h3>
                         </div>
                         <div className="mb-5 flex place-items-center">
                             <p className="mr-2 text-white/80 font-bold">4</p>
@@ -32,11 +33,11 @@ export const Card = () => {
                 </section>
 
                 {/* Bottom card */}
-                <section className=" rounded-br-xl rounded-bl-xl p-5 flex flex-col">
+                <section className=" rounded-br-xl rounded-bl-xl p-5 flex flex-col bg-white">
                     <p className="font-bold text-small tracking-wide mb-4">Profesor</p>
-                    <a className="border w-full text-center py-2 rounded-md font-bold flex justify-center">
+                    <a className="border w-full text-center py-3 rounded-md font-bold flex justify-center content-center">
                         <span>Button</span>
-                        <span className="ml-2 "> <FontAwesomeIcon icon={faArrowRight}/> </span> 
+                        <span className="ml-2 self-center"> <FontAwesomeIcon icon={faArrowRight}/> </span> 
                     </a>
                 
                 </section>
