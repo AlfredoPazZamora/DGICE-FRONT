16c9ee48475974d59b91a9a0b3e360759c9998bb
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2022-09-19 19:39:34 -0500

    faq-finished by yisus waiting por comments or recomendations by team

--- components/FaqSection/Faq.jsx
+++ components/FaqSection/Faq.jsx
@@ -4,7 +4,7 @@ import FooterVideo from '../FooterVideo'
 const Faq = () => {
   return (
     <>
-      <div className="xs:container sm:container lg:container py-6 ">
+      <div className="container py-6 ">
 
         <div className="text-center py-5 ">
           <p className="lg:text-5xl sm:text-4xl text-3xl my-5 font-sans-serif mx-4">
@@ -13,108 +13,130 @@ const Faq = () => {
           <hr className='mx-6' />
         </div>
 
-        {/* aqui empieza la card de las preguntas  */}
 
-        <div className="grid-cols-1  md:grid-cols-1 lg:grid-cols-3 mx-4 my-4 py-2">
+        <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2   mx-4 my-4 py-2">
+          {/* aqui empieza la card de las preguntas  */}
 
-          <div className=" ">
+          <div className="bg-white rounded-lg mx-2 my-2   ">
 
-            <div className="bg-white rounded-lg mx-2 my-2   ">
+            <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
 
-              <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
+              <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
 
-                <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
+                <p className=" text-sm font-bold">
+                  ¿Cómo me registro a la plataforma DGICE?
+                </p>
+              </div>
 
-                  <p className=" text-sm font-bold">
-                    ¿Cómo me registro a la plataforma DGICE?
+              <div className="py-5">
+                <ul className="mx-4 text-startfont-inter text-justify">
+                  <p >
+                    Para poder registrarte debes ser miembro de la universidad de colima.
                   </p>
-                </div>
-
-                <div className="py-5">
-                  <ul className="mx-4 text-startfont-inter text-justify">
-                    <p >
-                      Para poder registrarte debes ser miembro de la universidad de colima.
-                    </p>
-                    <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
-                  </ul>
-                </div>
-
+                  <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
+                </ul>
               </div>
 
             </div>
 
           </div>
+
 
           {/* aqui termina */}
           {/* aqui empieza la card de las preguntas  */}
 
-          <div className=" sm:grid-cols-1 grid-cols-1 md:grid-cols-1 lg:grid-cols-1 mx-4 my-4 py-2">
+          <div className="bg-white rounded-lg mx-2 my-2   ">
 
-            <div className="bg-white rounded-lg mx-2 my-2   ">
+            <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
 
-              <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
+              <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
 
-                <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
+                <p className=" text-sm font-bold">
+                  ¿Cómo me registro a la plataforma DGICE?
+                </p>
+              </div>
 
-                  <p className=" text-sm font-bold">¿Cómo me registro a la plataforma DGICE?</p>
-                </div>
-
-                <div className="py-5">
-                  <ul className="mx-4 text-startfont-inter text-justify">
-                    <p >
-                      Para poder registrarte debes ser miembro de la universidad de colima.
-                    </p>
-                    <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
-                  </ul>
-                </div>
-
+              <div className="py-5">
+                <ul className="mx-4 text-startfont-inter text-justify">
+                  <p >
+                    Para poder registrarte debes ser miembro de la universidad de colima.
+                  </p>
+                  <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
+                </ul>
               </div>
 
             </div>
 
           </div>
+
 
           {/* aqui termina */}
           {/* aqui empieza la card de las preguntas  */}
 
-          <div className=" sm:grid-cols-1 grid-cols-1 md:grid-cols-1 lg:grid-cols-1 mx-4 my-4 py-2">
+          <div className="bg-white rounded-lg mx-2 my-2   ">
 
-            <div className="bg-white rounded-lg mx-2 my-2   ">
+            <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
 
-              <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
+              <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
 
-                <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
+                <p className=" text-sm font-bold">
+                  ¿Cómo me registro a la plataforma DGICE?
+                </p>
+              </div>
 
-                  <p className=" text-sm font-bold">¿Cómo me registro a la plataforma DGICE?</p>
-                </div>
-
-                <div className="py-5">
-                  <ul className="mx-4 text-startfont-inter text-justify">
-                    <p >
-                      Para poder registrarte debes ser miembro de la universidad de colima.
-                    </p>
-                    <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
-                  </ul>
-                </div>
-
+              <div className="py-5">
+                <ul className="mx-4 text-startfont-inter text-justify">
+                  <p >
+                    Para poder registrarte debes ser miembro de la universidad de colima.
+                  </p>
+                  <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
+                </ul>
               </div>
 
             </div>
 
           </div>
 
+
           {/* aqui termina */}
+          {/* aqui empieza la card de las preguntas  */}
+
+          <div className="bg-white rounded-lg mx-2 my-2   ">
+
+            <div className=" rounded-lg my-5 mx-2 ring-2 ring-blue   ">
+
+              <div className=" bg-blue text-center font-sans-serif  rounded-t-lg py-2">
+
+                <p className=" text-sm font-bold">
+                  ¿Cómo me registro a la plataforma DGICE?
+                </p>
+              </div>
+
+              <div className="py-5">
+                <ul className="mx-4 text-startfont-inter text-justify">
+                  <p >
+                    Para poder registrarte debes ser miembro de la universidad de colima.
+                  </p>
+                  <p>Debes ingresar con tu numero de cuenta y contraseña.</p>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+
+
+          {/* aqui termina */}
+
         </div>
+
+
+
+
 
         <div className="text-center py-5 ">
           <hr className='mx-6' />
         </div>
-
-
-
-
-
-
 
 
 
