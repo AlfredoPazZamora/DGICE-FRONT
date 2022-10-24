429dba6642a7cb235f0b3608565eed73c99cc060
zaholy16 <mmondragon@ucol.mx>
2022-10-24 12:45:55 -0500

    animations

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -14,13 +14,13 @@ export const NavBar = () => {
                             <a>
                                 <Image
                                     src="/img/circle.png"
-                                    width={110}
-                                    height={110}
+                                    width={100}
+                                    height={100}
                                 />
                             </a>
                         </Link>
                     </div>
-                    <div className='text-xl mx-8'>
+                    <div className='text-xl mx-8 transform hover:underline'>
                         <Link href='/'>
                             <a>Cursos</a>
                         </Link>
@@ -32,7 +32,7 @@ export const NavBar = () => {
                         <input className='rounded-3xl border border-black focus-within:shadow-lg px-5 py-2.5 text-sm md:text-xs lg:w-72 shadow-md'  type="text" id='search' name='search' placeholder='Search' />
                     </div>
                     <div className='text-xs lg:text-sm mx-4'>
-                        <button className='border border-blue_green bg-blue_green focus-within:shadow-lg text-center hover:bg-blue_light transition duration-500 ease-in-out rounded-3xl px-8 md:px-12 py-2.5 shadow-md' type='button'>Ingresar</button>
+                        <button className='border border-blue_green bg-blue_green transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-80 rounded-3xl px-8 md:px-12 py-2.5 shadow-md' type='button'>Ingresar</button>
                     </div>
                 </div>
             </nav>
