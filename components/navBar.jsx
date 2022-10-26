8898e21ad519bcb290b1f8a21f1b6c7d53ae4b59
zaholy16 <mmondragon@ucol.mx>
2022-10-26 15:16:19 -0500

    login and logout

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -32,10 +32,17 @@ export const NavBar = () => {
                         <input className='rounded-3xl border border-black focus-within:shadow-lg px-5 py-2.5 text-sm md:text-xs lg:w-72 shadow-md'  type="text" id='search' name='search' placeholder='Search' />
                     </div>
                     <div className='text-xs lg:text-sm mx-4'>
-                        <button className='border border-blue_green bg-blue_green transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-80 rounded-3xl px-8 md:px-12 py-2.5 shadow-md' type='button'>Ingresar</button>
+                        <button className='border border-blue_green bg-blue_green transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-80 rounded-3xl px-8 md:px-12 py-2.5 shadow-md'type='button'>
+                        <a href="http://dgice-academy.ucol.mx:3000/login">Iniciar sesión</a>
+                        </button>
                     </div>
                 </div>
             </nav>
         </>
     );
 }
+
+// const redirects = () => {
+//     window.location.replace("http://dgice-academy.ucol.mx:3000/login")
+//     return;
+// }
