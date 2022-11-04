782d25f4df6a099d5ab3158db6f6c0e683c9521d
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2022-11-04 01:22:40 -0600

    adding same configurations that I used to at faq-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     screens: {
       'sm': '375px',
       'md': '500px',
@@ -16,7 +19,8 @@ module.exports = {
       'blue': "#31D0C1",
       'green': "#8DE137",
       'black': "#000000",
-      'white': "#FEFEFE"
+      'white': "#FEFEFE",
+      'gray': "#a1a1aa",
     },
     fontFamily: {
       inter: ["Inter", "sans-serif"],
@@ -29,7 +33,13 @@ module.exports = {
       'small': "1.2rem",
       'sm': '0.875rem',
       'xs': '0.75rem',
-      'xl': '1.25rem'
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
     },
     extend: {},
   },
