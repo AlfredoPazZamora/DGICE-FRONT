70765f494db3c61fd3d174770c5c4db523c04310
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2022-11-04 15:36:38 -0600

    fixing mindiwidthscreen

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-5">
+    <div className="min-w-screen min-h-screen flex flex-col justify-center items-center space-y-5">
       <p className="text-6xl text-black font-bold">404</p>
       <p className="text-3xl">Page not found</p>
       <p>The URL of the page was not found. Please try again</p>
