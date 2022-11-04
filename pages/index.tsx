073cb113be63f4d9683ffc40dc719f96b8d17f6b
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2022-11-04 14:05:25 -0600

    spinner de loading page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 
 import type { NextPage } from 'next'
-<<<<<<< HEAD
-import NavBar from '../components/navBar'
 import ErrorPage from '../components/errorPage'
-=======
+import LoadingPage from '../components/LoadingPage'
 import { NavBar } from '../components/NavBar'
 
-
->>>>>>> 49e6c063a01ccb1c40a93e6c05cb53938d1e2c12
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // Example to add an icon in your component
 // import { faCheck } from '@fortawesome/free-solid-svg-icons'
@@ -17,7 +13,8 @@ const Home: NextPage = () => {
   return (
     <>
       <NavBar></NavBar>
-      <ErrorPage></ErrorPage>
+      {/* <ErrorPage></ErrorPage> */}
+      <LoadingPage></LoadingPage>
     </>
   )
 }
