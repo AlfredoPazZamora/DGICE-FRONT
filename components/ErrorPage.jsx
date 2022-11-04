0723c73e0b7d91180d7054cbd42b744f3d3f79c3
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2022-11-04 14:50:55 -0600

    positions icon

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -6,11 +6,11 @@ import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const ErrorPage = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-">
+    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-3">
       <div className="flex flex-row items-center space-x-4">
+        <FontAwesomeIcon className="pt-2"size="2xl" icon={faTriangleExclamation} />
         <p className="text-6xl text-blue font-bold">404</p>
 
-        <FontAwesomeIcon className="pt-2"size="2xl" icon={faTriangleExclamation} />
       </div>
       <p className="text-3xl">Page not found</p>
       <p>The URL of the page was not found. Please try again.</p>
