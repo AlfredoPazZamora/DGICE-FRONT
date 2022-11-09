3183c39e0cb13da3bbbeed6d5739e7acba54f977
AlfredoPazZamora <apaz0@ucol.mx>
2022-11-09 16:00:53 -0600

    Fix: fix merge with error page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 
 import type { NextPage } from 'next'
-<<<<<<< HEAD
-import NavBar from '../components/navBar'
-import ErrorPage from '../components/errorPage'
-=======
 import { NavBar } from '../components/NavBar'
 
-
->>>>>>> 49e6c063a01ccb1c40a93e6c05cb53938d1e2c12
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // Example to add an icon in your component
 // import { faCheck } from '@fortawesome/free-solid-svg-icons'
@@ -17,7 +11,6 @@ const Home: NextPage = () => {
   return (
     <>
       <NavBar></NavBar>
-      <ErrorPage></ErrorPage>
     </>
   )
 }
