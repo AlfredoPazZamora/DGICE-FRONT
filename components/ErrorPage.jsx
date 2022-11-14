a7df3120a3a3bf5c1f20899f1c94aa61cec91d94
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2022-11-14 11:27:57 -0600

    translate the page to español y link del boton home

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 // import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
 
 
 const ErrorPage = () => {
   return (
-    <div className="min-w-screen pt-10 flex flex-col justify-center items-center space-y-3">
+    <div className="min-w-screen pt-10 flex flex-col justify-center items-center space-y-3 pt-20">
       <div className="flex flex-row items-center space-x-4">
-        <FontAwesomeIcon className="pt-2"size="2xl" icon={faTriangleExclamation} />
-        <p className="text-6xl text-blue font-bold">404</p>
+        <FontAwesomeIcon className="pt-2" size="2xl" icon={faTriangleExclamation} />
+        <p className="text-6xl text-blue font-bold">Error 404</p>
 
       </div>
-      <p className="text-3xl">Page not found</p>
-      <p>The URL of the page was not found. Please try again.</p>
+      <p className="text-3xl text-center">Página no encontrada</p>
+      <p className="text-center">La dirección de la pagina no fue encontrada. Porfavor intentalo de nuevo.</p>
 
       <div className="flex space-x-2">
-        <button className="p-2 bg-blue text-white rounded-md ">Go home</button>
+        <Link href="/">
+          <button className="p-2 bg-blue text-white rounded-md ">Ir al inicio</button>
+        </Link>
       </div>
     </div>
   )
